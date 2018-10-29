2973ce0921e654e13ec95fb54d79df0abef89e10
SuyuanLiu <suyuanliu0618@gmail.com>
2018-10-29 15:32:54 +0800

    add find nums[i]==i

--- Python3/BinarySearch/search.py
+++ Python3/BinarySearch/search.py
@@ -1,24 +1,36 @@
 # -*- coding:UTF-8 -*-
 '''
 Question：
-给定有序数组arr,其中不含有重复元素，请找到满足arr[i]==i条件的最左的位置。
+给定有序数组nums,其中不含有重复元素，请找到满足nums[i]==i条件的最左的位置。
 如果所有位置上的数都不满足条件，返回-1.
 
 Solution：
 - 二分搜索；
-- 首先定义res = -1, 然后利用二分搜索，如果arr[i] > i, 那么结果在i的左侧，因为整数的增幅最小是1，i的增幅严格为1，右侧不可能存在相等情况；
-                                      若arr[i] < i, 那么结果在i的右侧;
-                                      若arr[i] == i，把i赋值给res,然后继续搜索左侧，因为要找的是最左的位置；                                   
+- 首先定义res = -1, 然后利用二分搜索，如果nums[i] > i, 那么结果在i的左侧，因为整数的增幅最小是1，i的增幅严格为1，右侧不可能存在相等情况；
+                                      若nums[i] < i, 那么结果在i的右侧;
+                                      若nums[i] == i，把i赋值给res,然后继续搜索左侧，因为要找的是最左的位置；                                   
 - 如果存在以下两种特殊情况，直接返回-1：
-    arr[0] > 0 和 arr[n-1] < n-1 （n = len(arr)）                                       
+    nums[0] > 0 和 nums[n-1] < n-1 （n = len(nums)）                                       
 '''
 class Solution:
-    def search(self, arr):
+    def search(self, nums):
         '''
-        :type arr: List[int]
+        :type nums: List[int]
         :rtype: int
         '''
-        n = len(arr)
-        if n == 0:
+        n = len(nums)
+        if n == 0 or nums[0] > 0 or nums[n-1] < n-1:
             return -1
         
+        res = -1
+        start, end = 0, n-1
+        while start <= end:
+            mid = (start + end) // 2
+            if mid == nums[mid]:
+                res = mid
+                end = mid - 1
+            elif mid > nums[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return res
--- README.md
+++ README.md
@@ -15,11 +15,11 @@ If labels is :octocat: , it means it's a classic question.
 
 ## BinarySearch
 ||Questions|Solution|info|lables|
-|---|--------|:-------:|:--------:|--------|
+|---|--------|:-------:|:--------:|:-----:|
 |0|[Binary Search](https://leetcode.com/problems/binary-search/)| [Python3](Python3/BinarySearch/binarySearch.py)|标准二分|:octocat:|
-|1|[Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays/)| [Python3](Python3/BinarySearch/intersection.py)|二分法TODO||
-|2|[Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/)| [Python3](Python3/BinarySearch/intersection2.py)|二分法TODO||
+|1|[Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays/)| [Python3](Python3/BinarySearch/intersection.py)|二分法**TODO**||
+|2|[Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/)| [Python3](Python3/BinarySearch/intersection2.py)|二分法**TODO**||
 |3|[Pow(x,n)](https://leetcode.com/problems/powx-n/)| [Python3](Python3/BinarySearch/myPow.py)|利用二分思想|:octocat:|
 |4|[Count Complete Tree Nodes](https://leetcode.com/problems/count-complete-tree-nodes/)| [Python3](Python3/BinarySearch/countNodes.py)|利用二分思想|:octocat:|
-|5|查找arr[i]==i|[Python3](Python3/BinarySearch/search.py)|二分搜索TODO|:octocat:|
+|5|find nums[i]==i|[Python3](Python3/BinarySearch/search.py)|二分搜索TODO|:octocat:|
 
