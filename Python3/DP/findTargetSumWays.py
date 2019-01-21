8574f0978c1e1fb4978566ed291e44f32a1d71a6
SuyuanLiu <suyuanliu0618@gmail.com>
2019-01-21 22:12:13 +0800

    add Target Sum

---
+++ Python3/DP/findTargetSumWays.py
@@ -0,0 +1,43 @@
+# -*- coding:UTF-8 -*-
+'''
+Solution 1: DP, TC-O(NC), SC-O(C)
+- !!! (这种方法在数字值非常大的时候很耗空间，没有通过LeetCode测试)
+- 转换为0-1背包问题
+- 把列表中的数分为两部分，P(positive), N(negative)，有如下推导：
+  sum(P) - sum(N) = target
+  sum(P) - sum(N) + sum(P) + sum(N) = target + sum(nums)
+  2sum(P) = target + sum(nums)
+- 也就是要找一个子集，使得其和为 n = (target + sum(nums)) / 2. 
+- 定义一个数组dp[len(nums)+1][n+1]，其中dp[i][j]指的是前i个值中组成和为j的方法数，那么
+  dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j]  (前i-1个数组成j-n的方法数(用nums[i-1]) + 前i-1组成j的方法数)
+- 注意使用一维的dp时，要倒着更新，防止被覆盖。
+
+
+Solution 2: DP
+- 参考高票答案
+- 
+'''
+
+# Solution 1
+class Solution:
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        if (sum(nums) + S) % 2:
+            return 0
+        
+        n = (sum(nums) + S) // 2
+        dp = [0 for i in range(n+1)]
+        dp[0] = 1
+        
+        for i in range(1, len(nums)+1):
+            for j in range(n, 0, -1):
+                if j >= nums[i-1]:
+                    dp[j] += dp[j-nums[i-1]]
+                    
+        return dp[n]
+                
+                
--- README.md
+++ README.md
@@ -159,6 +159,7 @@ If labels is :octocat: , it means it's a classical question.
 |0|[Longest Increasing Subsequence](https://leetcode.com/problems/longest-increasing-subsequence/)| [Python3](Python3/DP/lengthOFLIS.py)|TODO:O(nlogn)参考链接在代码里|:octocat:百度|
 |1|[背包问题] | [Python3](Python3/DP/knapsack.py)|**0-1背包**题目描述在代码内|:octocat:|
 |2|[Partition Equal Subset Sum](https://leetcode.com/problems/partition-equal-subset-sum/)| [Python3](Python3/DP/canPartition.py)|**0-1背包**|:octocat:|
+|3|[Target Sum](https://leetcode.com/problems/target-sum/)| [Python3](Python3/DP/findTargetSumWays.py)|**0-1背包, DFS**|:octocat:|
 
 [Back to Top](#index)
 
