720dad6a37993fbd46bb079f92fa640837405913
SuyuanLiu <suyuanliu0618@gmail.com>
2019-01-23 10:53:31 +0800

    update binary tree

--- Python3/BinaryTree/inorderTraversal.py
+++ Python3/BinaryTree/inorderTraversal.py
@@ -3,13 +3,13 @@
 Solution: 中序遍历
 - 中序遍历顺序： 左 根 右
 
+Solution 2
+- 高票答案的一个简化版，更清晰一点；
+
 Solution 1
 - 定义一个栈，把root压入，遍历所有左孩子，压入左孩子；
     - 再依次弹出，看是否有右孩子，有的话，再重复前面的操作，把它的左孩子全放进去；
     - 没有右节点的话，就接着弹出栈的内容；
-
-Solution 2
-- 高票答案的一个简化版，更清晰一点；
 '''
 
 # Definition for a binary tree node.
@@ -19,6 +19,28 @@ Solution 2
 #         self.left = None
 #         self.right = None
 
+# Solution2: 简化一点的
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        res, stack = [], []
+        p = root
+        while p or stack:
+            while p:
+                stack.append(p)
+                p = p.left
+            if stack:
+                temp = stack.pop()
+                res.append(temp.val)
+                p = temp.right
+        return res
+# Solution 1
 class Solution:
     def inorderTraversal(self, root):
         """
@@ -50,24 +72,4 @@ class Solution:
 
 
 
-# Solution2: 简化一点的
-class Solution:
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        if not root:
-            return []
-        
-        res, stack = [], []
-        p = root
-        while p or stack:
-            while p:
-                stack.append(p)
-                p = p.left
-            if stack:
-                temp = stack.pop()
-                res.append(temp.val)
-                p = temp.right
-        return res
+
