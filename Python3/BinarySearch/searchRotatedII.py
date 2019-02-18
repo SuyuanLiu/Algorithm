e938ddaad6d49809fd1f6f5cd5a02327331fbd62
SuyuanLiu <suyuanliu0618@gmail.com>
2019-02-18 18:33:36 +0800

    add: for loop solution

--- Python3/BinarySearch/searchRotatedII.py
+++ Python3/BinarySearch/searchRotatedII.py
@@ -6,7 +6,20 @@ Solution:
 - 如果发现 nums[start] == nums[mid]，就把start加1，相当于去掉重复元素的影响；
 - 然后再判断是哪一侧有序，判断target是否在有序的那一侧；
 - 对重复元素的解决办法就是移动边缘指针，相当于去掉影响有序判读的重复元素，导致每次不能去掉一半的元素，导致时间复杂度增加到O(N).
+
+Solution 2:
+- for loop, 遍历
 '''
+# Solution 2
+class Solution:
+    def search(self, nums: 'List[int]', target: 'int') -> 'bool':
+        for n in nums:
+            if n == target:
+                return True
+        return False
+        
+
+# Solution 1
 class Solution:
     def search(self, nums, target):
         """
