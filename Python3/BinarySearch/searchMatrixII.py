4a2bc2d2e63beea819b0df3e9b27de1ffbdcb126
SuyuanLiu <suyuanliu0618@gmail.com>
2019-02-18 15:44:47 +0800

    add: follow up for search matrix II

--- Python3/BinarySearch/searchMatrixII.py
+++ Python3/BinarySearch/searchMatrixII.py
@@ -3,6 +3,13 @@
 Solution:
 - 二分思想
 - 从矩阵的右上角的点开始比较，如果target比它大，说明target在下面的行(i += 1)，小则在前面的列(j -= 1)
+- 或者从左下角的点开始找。
+
+Follow up:
+Question: https://www.lintcode.com/problem/search-a-2d-matrix-ii/description
+如果数组中有重复的值，找出target出现了几次；
+Solution: 
+从右上角开始，如果target比它大，就往下走；小就往左走；相等则向左下角走；
 '''
 class Solution:
     def searchMatrix(self, matrix, target):
@@ -24,3 +31,29 @@ class Solution:
             else:
                 j -= 1
         return False
+
+
+# Follow up Solution
+class Solution:
+    """
+    @param matrix: A list of lists of integers
+    @param target: An integer you want to search in matrix
+    @return: An integer indicate the total occurrence of target in the given matrix
+    """
+    def searchMatrix(self, matrix, target):
+        # write your code here
+        if not matrix or not matrix[0]:
+            return 0
+            
+        row, col = len(matrix), len(matrix[0])
+        cnt = 0
+        i, j = 0, col - 1 
+        while 0 <= i < row and 0 <= j < col:
+            if target == matrix[i][j]:
+                cnt += 1 
+                i, j = i + 1, j - 1 
+            elif target > matrix[i][j]:
+                i += 1 
+            else:
+                j -= 1 
+        return cnt 
