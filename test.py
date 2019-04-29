768b76134f7242b285dd362daaf448588abf5e7b
SuyuanLiu <suyuanliu0618@gmail.com>
2019-04-29 18:11:16 +0800

    add: Leetcode/Subsets, DFS solution

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
+class Solution:
+    def subsets(self, nums):
+        res = []
+        self.dfs(nums, [], 0, res)
+        return res
+        
+        
+    def dfs(self, nums, subset, start, res):
+        res.append([c for c in subset])
+        # import pdb; pdb.set_trace()
+        
+        for i in range(start, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, subset, i+1, res)
+            subset.pop()
+            
+            
+s = Solution()
 nums = [1,2,3]
-for i in range(len(nums)):
-    print(nums[i+1:])
-print(nums[4:])
+print(s.subsets(nums))
